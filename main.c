10c98f6e5dd983ebedafe421e2d9827c07c81af8
KangHe <hekangbit@163.com>
2018-12-20 22:48:27 +0800

    This example code can print string in matrix order.

---
+++ main.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <malloc.h>
+#include <string.h>
+#include <stdlib.h>
+
+int str_reorder(char* praw, char* pout, int k)
+{
+        if(praw == NULL || pout == NULL)
+        {
+                return -1;
+        }
+
+        if(k < 1)
+                return -1;
+
+
+        int tmp_mod = 2 * k - 2;
+        int i = 0;
+        int j = 0;
+        int len = strlen(praw);
+
+        if(tmp_mod == 0)
+        {
+                memcpy(pout,praw,len);
+                return 0;
+        }           
+
+        for(i=0;i<k;i++)
+        {
+                for(j=0;j<len;j++)
+                {
+                        if(j % tmp_mod == i || j % tmp_mod == 2*k-2-i)
+                        {
+                                *(pout++) = *(praw+j);
+                        }
+                }
+        }
+        return 0;
+
+
+}
+int main(int argc, char** argv)
+{
+        
+        char *praw;
+        int row;
+        char *pout;
+        int len;
+
+        if(argv[1]==NULL || argv[2]==NULL)
+        {
+                printf("input error\n");
+                return 0;
+        }
+
+        praw = argv[1];
+        row = atoi(argv[2]);
+
+        printf("The raw stirng is %s\n", praw);
+        printf("The row is %d\n", row);
+
+        len = strlen(praw);
+        pout = malloc(len + 1);
+
+        memset(pout, 0x00, len + 1);
+
+        str_reorder(praw, pout, row);
+
+        printf("the final ouput is\n%s\n", pout);
+
+        free(pout);
+        return 0;
+}
+
