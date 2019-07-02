b0904e6b02629ecfd72e5ced55c45cba04830ef7
kangh <hekangbit@163.com>
2019-07-03 00:11:38 +0800

    Add maopao sort and bsearch

--- sort.c
+++ sort.c
@@ -68,12 +68,58 @@ void print_buf(int buf[], int size)
 	printf("\n");
 }
 
+void maopao_sort(int arr[], int n)
+{
+	int i = 0;
+	int j = 0;
+	int tmp;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = 0; j < n - i - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+	}
+}
+
+int bsearch(int arr[], int n, int target)
+{
+	int low = 0;
+	int high = n - 1;
+	int mid;
+	int tmp;
+
+	while (low <= high)
+	{
+		mid = (high + low) / 2;
+		tmp = arr[mid];
+		if (target == tmp)
+			return mid;
+		else if(target < tmp)
+			high = mid - 1;
+		else if(target > tmp)
+			low = mid + 1;
+	}
+	return 0;
+}
+
 int main()
 {
-	int test[10] = {11,100,5,4,78,1,23,14,2,45};
-	print_buf(test, 10);
-	quick_sort(test, 0, 9);
-	print_buf(test, 10);
+	int test[] = {1,11,100,5,4,78,11,23,14,2,45};
+	print_buf(test, sizeof(test)/sizeof(int));
+	quick_sort(test, 0, sizeof(test)/sizeof(int) - 1);
+	print_buf(test, sizeof(test)/sizeof(int));
 	
+	int test2[] = {1,11,100,5,4,78,11,23,14,2,45};
+	print_buf(test2, sizeof(test)/sizeof(int));
+	maopao_sort(test2, sizeof(test)/sizeof(int));
+	print_buf(test2, sizeof(test)/sizeof(int));
+	printf("find 23, index is %d", bsearch(test2, sizeof(test)/sizeof(int), 23));
 	return 0;
 }
